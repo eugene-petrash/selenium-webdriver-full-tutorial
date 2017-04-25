c46ce6075136386e17dab901fc44130b6e5e2a44
Eugene Petrash <petrash.evgeniy@gmail.com>
2017-04-26 00:17:44 +0300

    Capabilities and command line options(Chrome)

--- python-example/test_login.py
+++ python-example/test_login.py
@@ -3,39 +3,35 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
-from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
 
 @pytest.fixture
 def driver(request):
-    wd = webdriver.Firefox(capabilities={'marionette': True,
-                                         'acceptSslCerts': False,
-                                         'applicationCacheEnabled': False,
-                                         'takesScreenshot': True,
-                                         'version': '45.9.0',
-                                         'raisesAccessibilityExceptions': False,
-                                         'platform': 'LINUX',
-                                         'platformName': 'Linux',
-                                         'proxy': {},
-                                         'platformVersion': '4.4.0-72-generic',
-                                         'browserName': 'Firefox',
-                                         'specificationLevel': '1',
-                                         'XULappId': '{ec8030f7-c20a-464f-9b0e-13a3a9e97384}',
-                                         'takesElementScreenshot': True,
-                                         'browserVersion': '45.9.0',
-                                         'device': 'desktop',
-                                         'rotatable': False,
-                                         'appBuildId': '20170411115307'
-                                         },
-                           firefox_binary=FirefoxBinary("/usr/local/bin/firefox_ESR_45/firefox"),
-                           firefox_profile=FirefoxProfile(),
-                           timeout=50,
-                           proxy=None,
-                           executable_path='geckodriver',
-                           firefox_options=None, # More details here https://seleniumhq.github.io/selenium/docs/api/py/_modules/selenium/webdriver/firefox/options.html
-                           log_path='geckodriver.log'
-                           # More details here http://seleniumhq.github.io/selenium/docs/api/py/webdriver_firefox/selenium.webdriver.firefox.webdriver.html#module-selenium.webdriver.firefox.webdriver
-                           )
+    wd = webdriver.Chrome(desired_capabilities={'chromeOptions': {'args': ['--start-fullscreen'],  # More here http://peter.sh/experiments/chromium-command-line-switches/
+                                                                  'binary': '/usr/bin/google-chrome'},
+                                                'databaseEnabled': False,
+                                                'platform': 'Linux',
+                                                'networkConnectionEnabled': False,
+                                                'takesScreenshot': True,
+                                                'javascriptEnabled': True,
+                                                'browserName': 'chrome',
+                                                'hasTouchScreen': False,
+                                                'takesHeapSnapshot': True,
+                                                'pageLoadStrategy': 'normal',
+                                                'webStorageEnabled': True,
+                                                'applicationCacheEnabled': False,
+                                                'rotatable': False,
+                                                'chrome': {'userDataDir': '/tmp/.org.chromium.Chromium.ayvBAD',
+                                                           'chromedriverVersion': '2.29.461571 (8a88bbe0775e2a23afda0ceaf2ef7ee74e822cc5)'},
+                                                'handlesAlerts': True,
+                                                'mobileEmulationEnabled': False,
+                                                'locationContextEnabled': True,
+                                                'acceptSslCerts': True,
+                                                'version': '57.0.2987.133',
+                                                'browserConnectionEnabled': False,
+                                                'unexpectedAlertBehaviour': 'ignore', # ignore/accept/dismiss
+                                                'cssSelectorsEnabled': True,
+                                                'nativeEvents': True
+                                                })
     print(wd.capabilities)
     request.addfinalizer(wd.quit)
     return wd
